19167ffe5e24565349e4935cea2cad7e9902f5fa
lm7272 <lewis.a.mead@gmail.com>
2023-04-11 18:37:57 +0100

    boundary matrix

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,18 @@
-use nalgebra::{DMatrix};
 mod simplicial_complex{
     pub mod simplicial_complex;
 }
 use simplicial_complex::simplicial_complex::{Facet, Simplex, SimplicialComplex};
 
-fn compute_boundary_matrix(k_simplices: &Vec<Facet>, k_plus_one_simplices: &Vec<Facet>) -> DMatrix<i32> {
-    let m = k_simplices.len();
-    let n = k_plus_one_simplices.len();
-    let mut matrix = DMatrix::from_element(m, n, 0);
-    // Populate the matrix based on whether k-simplices are in the boundary of a k+1 simplex
-    for (i, facet) in k_plus_one_simplices.iter().enumerate() {
-        let bdy = facet.boundary();
-        for simplex in bdy {
-            let index = k_simplices.iter().position(|x| simplex == x);
-            if let Some(j) = index {
-                matrix[(j, i)] = 1
-            }
-        }
-    }
-
-    matrix
-}
-
 fn main() {
-    let facets: Vec<Facet> = vec![Simplex::new(vec![0,1]), Simplex::new(vec![1,2]), Simplex::new(vec![0,2])];
-    let bdy: Vec<Facet> = vec![Simplex::new(vec![0]), Simplex::new(vec![1]), Simplex::new(vec![2])];
-    let m = compute_boundary_matrix(&bdy, &facets);
+    let facets: Vec<Facet> = vec![Simplex::new((0..6).collect())];
+    let sc:SimplicialComplex = SimplicialComplex::new(facets);
+    sc.print();
+    let dim = 3;
+    let faces = sc.k_faces(dim);
+    for face in faces{
+        face.print();
+    }
+    let m = sc.compute_k_boundary_matrix(dim);
     println!("{}", m);
 }
 
--- src/simplicial_complex/simplicial_complex.rs
+++ src/simplicial_complex/simplicial_complex.rs
@@ -1,3 +1,8 @@
+use nalgebra::DMatrix;
+use itertools::Itertools;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+
 pub trait Simplex {
     type Boundary: Sized;
     fn new(vertices: Vec<usize>) -> Self;
@@ -24,6 +29,14 @@ impl PartialEq for Facet {
 impl PartialEq<&Facet> for Facet {
     fn eq(&self, other: &&Facet) -> bool {
         self.vertices == other.vertices
+    }
+}
+
+impl Eq for Facet {}
+
+impl Hash for Facet {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.vertices.hash(state);
     }
 }
 
@@ -63,25 +76,53 @@ impl Simplex for Facet{
     }
 
     fn print(&self) {
-        // let n: usize = self.dimension();
         println!("{:?}", self.vertices);
-        // This has boundary simplices:", self.vertices, n);
-        // let _boundary: Vec<Self> = self.boundary();
-        // for face in _boundary {
-        //     println!("{:?}", face.vertices)
-        // }
     }
 }
 
 impl SimplicialComplex {
-    fn new(facets: Vec<Facet>) -> Self {
+    pub fn new(facets: Vec<Facet>) -> Self {
         Self { facets }
     }
 
-    fn print(&self) {
-        println!("Simplicial Complex:");
+    pub fn print(&self) {
+        println!("Simplicial Complex has dimension {}. The facets are:", self.dimension());
         for facet in &self.facets {
             facet.print();
         }
+    }
+
+    pub fn dimension(&self) -> usize{
+        self.facets.iter().map(|v| v.dimension()).max().unwrap()
+    }
+
+    pub fn k_faces(&self, dim: usize) -> Vec<Facet>{
+        let mut k_faces: Vec<Facet> = vec![];
+        for facet in &self.facets {
+            k_faces.extend(facet.vertices.iter()
+            .combinations(dim+1).map(|c| Facet::new(c.into_iter().copied().collect_vec())))
+        }
+        let k_faces_set: HashSet<Facet> = k_faces.into_iter().collect();
+        k_faces_set.into_iter().collect()
+    }
+
+    pub fn compute_k_boundary_matrix(&self, dim:usize) -> DMatrix<i32> {
+        let k_minus_one_simplices: Vec<Facet> = self.k_faces(dim-1);
+        let k_simplices: Vec<Facet> = self.k_faces(dim);
+        let m = k_minus_one_simplices.len();
+        let n = k_simplices.len();
+        let mut matrix = DMatrix::from_element(m, n, 0);
+        // Populate the matrix based on whether k-simplices are in the boundary of a k+1 simplex
+        for (i, facet) in k_simplices.iter().enumerate() {
+            let bdy = facet.boundary();
+            for simplex in bdy {
+                let index = k_minus_one_simplices.iter().position(|x| simplex == x);
+                if let Some(j) = index {
+                    matrix[(j, i)] = 1
+                }
+            }
+        }
+    
+        matrix
     }
 }
