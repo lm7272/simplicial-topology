639524168fec0333eada7dc53249e01a74f8674c
lm7272 <lewis.a.mead@gmail.com>
2023-04-12 18:49:06 +0100

    gaussian elimination

--- src/linear_algebra/smith_normal_form.rs
+++ src/linear_algebra/smith_normal_form.rs
@@ -1,20 +1,31 @@
-use nalgebra::DMatrix;
-pub fn pivot_rows_and_cols(a: &DMatrix<i32>, x: usize) -> DMatrix<i32>{
-    let (k, l) = match (x..a.nrows()).flat_map(|i| (x..a.ncols()).map(move |j| (i, j))).find(|&(i, j)| a[(i, j)] == 1) {
-        Some((i, j)) => (i, j),
-        None => return a.clone(),
-    };
-    let mut b = a.clone();
-    println!("{}, {}", k, l);
-    b.swap_rows(x, k);
-    b.swap_columns(x, l);
-    b
-}
+use std::ops::AddAssign;
 
-// pub fn func(a: DMatrix<i32>, x: usize) -> DMatrix<i32>{
-//     for i in x..a.nrows(){
-//         if a[(i,x)] == 1{
-//             a.row(i) += a.row(x);
-//         }
-//     }
-// }
+use nalgebra::DMatrix;
+pub fn gaussian_elimination(mut a: DMatrix<i32>) -> DMatrix<i32>{
+    for x in 0..a.nrows().max(a.ncols()){
+        // if there exists some i/geq x and j\geq x s.t. a[i,j] = 1 then swap rows x and i and swap cols x and j
+        match (x+1..a.nrows()).flat_map(|i| (x..a.ncols()).map(move |j| (i, j))).find(|&(i, j)| a[(i, j)] == 1) {
+            Some((i, j)) => {
+                a.swap_rows(x, i);
+                a.swap_columns(x, j);
+            },
+            None => return a,
+        };
+        for i in x+1..a.nrows(){
+            if a[(i,x)] == 1{
+                let row_x = a.row(x).clone_owned();
+                a.row_mut(i).add_assign(row_x);
+                a.row_mut(i).iter_mut().for_each(|n| *n %= 2);
+            }
+        }
+        for i in x+1..a.ncols(){
+            if a[(x,i)] == 1{
+                let col_x = a.column(x).clone_owned();
+                a.column_mut(i).add_assign(col_x);
+                a.column_mut(i).iter_mut().for_each(|n| *n %= 2);
+            }
+        }
+
+    }
+    a
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod linear_algebra{
     pub mod smith_normal_form;
 }
 use simplicial_complex::simplicial_complex::{Facet, Simplex, SimplicialComplex};
-use linear_algebra::smith_normal_form::pivot_rows_and_cols;
+use linear_algebra::smith_normal_form::{gaussian_elimination};
 
 fn main() {
     let facets: Vec<Facet> = vec![Simplex::new((0..4).collect())];
@@ -13,12 +13,9 @@ fn main() {
     let sc:SimplicialComplex = SimplicialComplex::new(bdy);
     sc.print();
     let dim = 2;
-    let faces = sc.k_faces(dim);
-    for face in faces{
-        face.print();
-    }
     let m = sc.compute_k_boundary_matrix(dim);
     println!("The {}-dimensional boundary operator is:{}", dim, m);
-    println!("The {}-dimensional boundary operator is:{}", dim, pivot_rows_and_cols(&m, 0));
+    //println!("The pivoted boundary operator is:{}", pivot_rows_and_cols(&m, 0));
+    println!("The gaussian eliminated boundary operator is:{}", gaussian_elimination(m));
     println!("This simplicial complex has Euler characteristic: {}", sc.euler_characteristic());
 }
