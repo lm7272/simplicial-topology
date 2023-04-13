2beb3015f01cd986cddb1447decc7c49b3e7caa3
lm7272 <lewis.a.mead@gmail.com>
2023-04-13 21:08:05 +0100

    betti number start

--- src/linear_algebra/smith_normal_form.rs
+++ src/linear_algebra/smith_normal_form.rs
@@ -1,31 +1,54 @@
 use std::ops::AddAssign;
-
 use nalgebra::DMatrix;
-pub fn gaussian_elimination(mut a: DMatrix<i32>) -> DMatrix<i32>{
-    for x in 0..a.nrows().max(a.ncols()){
+
+pub fn gaussian_elimination(mut matrix: DMatrix<i32>) -> DMatrix<i32>{
+    for x in 0..matrix.nrows().max(matrix.ncols()){
         // if there exists some i/geq x and j\geq x s.t. a[i,j] = 1 then swap rows x and i and swap cols x and j
-        match (x+1..a.nrows()).flat_map(|i| (x..a.ncols()).map(move |j| (i, j))).find(|&(i, j)| a[(i, j)] == 1) {
+        match (x+1..matrix.nrows()).flat_map(|i| (x..matrix.ncols()).map(move |j| (i, j))).find(|&(i, j)| matrix[(i, j)] == 1) {
             Some((i, j)) => {
-                a.swap_rows(x, i);
-                a.swap_columns(x, j);
+                matrix.swap_rows(x, i);
+                matrix.swap_columns(x, j);
             },
-            None => return a,
+            None => return matrix,
         };
-        for i in x+1..a.nrows(){
-            if a[(i,x)] == 1{
-                let row_x = a.row(x).clone_owned();
-                a.row_mut(i).add_assign(row_x);
-                a.row_mut(i).iter_mut().for_each(|n| *n %= 2);
+        for i in x+1..matrix.nrows(){
+            if matrix[(i,x)] == 1{
+                let row_x = matrix.row(x).clone_owned();
+                matrix.row_mut(i).add_assign(row_x);
+                matrix.row_mut(i).iter_mut().for_each(|n| *n %= 2);
             }
         }
-        for i in x+1..a.ncols(){
-            if a[(x,i)] == 1{
-                let col_x = a.column(x).clone_owned();
-                a.column_mut(i).add_assign(col_x);
-                a.column_mut(i).iter_mut().for_each(|n| *n %= 2);
+        for i in x+1..matrix.ncols(){
+            if matrix[(x,i)] == 1{
+                let col_x = matrix.column(x).clone_owned();
+                matrix.column_mut(i).add_assign(col_x);
+                matrix.column_mut(i).iter_mut().for_each(|n| *n %= 2);
             }
         }
 
     }
-    a
+    matrix
+}
+
+pub fn rank(smith_normal_matrix: DMatrix<i32>) -> usize{
+    let n = smith_normal_matrix.nrows().min(smith_normal_matrix.ncols());
+    let eliminated_matrix = gaussian_elimination(smith_normal_matrix);
+    let mut running_rank = 0;
+    for i in 0..n{
+        if eliminated_matrix[(i,i)] != 0{
+            running_rank += 1;
+        }
+        else {
+            return running_rank
+        }
+    }
+    running_rank
+}
+
+pub fn row_nullity(matrix: DMatrix<i32>) -> usize{
+    matrix.nrows() - rank(matrix)
+}
+
+pub fn columns_nullity(matrix: DMatrix<i32>) -> usize{
+    matrix.ncols() - rank(matrix)
 }
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,18 @@ mod linear_algebra{
     pub mod smith_normal_form;
 }
 use simplicial_complex::simplicial_complex::{Facet, Simplex, SimplicialComplex};
-use linear_algebra::smith_normal_form::{gaussian_elimination};
+use linear_algebra::smith_normal_form::{gaussian_elimination, rank, row_nullity};
 
 fn main() {
     let facets: Vec<Facet> = vec![Simplex::new((0..4).collect())];
     let bdy: Vec<Facet> = facets[0].boundary();
     let sc:SimplicialComplex = SimplicialComplex::new(bdy);
-    sc.print();
+    sc.print(); 
     let dim = 2;
-    let m = sc.compute_k_boundary_matrix(dim);
-    println!("The {}-dimensional boundary operator is:{}", dim, m);
-    //println!("The pivoted boundary operator is:{}", pivot_rows_and_cols(&m, 0));
-    println!("The gaussian eliminated boundary operator is:{}", gaussian_elimination(m));
+    let m1 = sc.compute_k_boundary_matrix(dim);
+    let m2 = sc.compute_k_boundary_matrix(dim+1);
+    println!("The {}-dimensional boundary operator is:{}", dim, m1);
+    println!("The {}-dimensional boundary operator is:{}", dim+1, m2);
+    println!("The {}-Betti number is: {}", dim, row_nullity(m2) - rank(m1));
     println!("This simplicial complex has Euler characteristic: {}", sc.euler_characteristic());
 }
